692110d13bcebc3924aa6afc1e3823b365540b59
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-09-25 23:32:42 +0200

    specifications and plots
    Add some specification about the priors we're using, and add basic plots
    for testing the models

--- analyses/defnite.R
+++ analyses/defnite.R
@@ -23,10 +23,22 @@ display(blr.rrh)
 # regular GLMs for quasi separation, it doesn't allow us to check for certain 
 # important concepts in Bayesian Statistics, such as the 95% Credible Interval.
 # Therefore, we will compute a fullfledged Bayesian GLM with NUTS:
-brm.wolf <- brm(wolf ~ timebin + rrh + opening, data=df, family="bernoulli")
+
+# We have no real a priori confidence that the estimates will be close to zero. 
+# As such a normal distribution with a relatively large scale would be appropriate
+# a a prior, or we could use distributions with heavier tails (do deal with potential
+# outliers), such as the Student t distribution. Being on the uninformed side, 
+# we'll use a unit student-t prior with 10 degrees of freedom for both models.
+brm.wolf <- brm(wolf ~ timebin + rrh + opening, data=df, 
+                prior=c(set_prior("student_t(10,0,1)", class="b")), 
+                family="bernoulli", iter=10000)
 summary(brm.wolf)
+plot(brm.wolf)
+marginal_effects(brm.wolf)
 
-brm.rrh <- brm(rrh ~ timebin + wolf + opening, data=df, family="bernoulli")
+brm.rrh <- brm(rrh ~ timebin + wolf + opening, data=df, 
+               prior=c(set_prior("student_t(10,0,1)", class="b")), 
+               family="bernoulli", iter=10000)
 summary(brm.rrh)
-
-# TODO: Further analyses and model criticism
+plot(brm.rrh)
+marginal_effects(brm.rrh)
