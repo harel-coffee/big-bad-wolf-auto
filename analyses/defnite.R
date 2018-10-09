39afff79fbb14e5dcbf73f963a4bd34121aae137
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-10-09 15:05:37 +0200

    updates

--- analyses/defnite.R
+++ analyses/defnite.R
@@ -8,6 +8,7 @@ df <- read.csv("defnite.csv")
 df$rrh = as.factor(df$rrh)
 df$wolf = as.factor(df$wolf)
 df$opening = as.factor(df$opening)
+#df$timebin = ordered(df$timebin)
 head(df)
 
 ## Compute simple Generalized Linear models
@@ -15,16 +16,14 @@ lr_wolf <- glm(wolf ~ timebin, data = df, family = "binomial")
 summary(lr_wolf)
 
 # This can't be done, because of quasi-separation problems (cf. the errors)
-lr_rrh <- glm(rrh ~ timebin + opening,  data = df, family = "binomial")
+lr_rrh <- glm(rrh ~ timebin,  data = df, family = "binomial")
 summary(lr_rrh)
 
 ## Compute Bayes Generalized Linear Models according to Gelman et al.
-blr_wolf <- bayesglm(wolf ~ timebin,
-                     data = df, family = "binomial")
+blr_wolf <- bayesglm(wolf ~ timebin, data = df, family = "binomial")
 display(blr_wolf)
 
-blr_rrh <- bayesglm(rrh ~ timebin,
-                    data = df, family = "binomial")
+blr_rrh <- bayesglm(rrh ~ timebin, data = df, family = "binomial")
 display(blr_rrh)
 
 # While Gelman's analysis using an EM optimalization overcomes the problem of 
@@ -39,14 +38,14 @@ display(blr_rrh)
 # we'll use a weakly-informative prior in the form of a unit student-t prior with 
 # 10 degrees of freedom for both models.
 brm_wolf <- brm(wolf ~ timebin, data = df,
-                prior = c(set_prior("student_t(10,0,1)", class = "b")),
-                family = "bernoulli", iter = 10000)
+                prior = c(set_prior("student_t(7,0,1)", class = "b")),
+                family = "bernoulli", iter = 10000, sample_prior = TRUE)
 summary(brm_wolf)
 plot(brm_wolf)
 
 # Next, what is the probability that timebin has no positive effect?
 mean(posterior_samples(brm_wolf, "timebin") < 0)
-hypothesis(brm_wolf, "timebin = 0") # alternatively
+hypothesis(brm_wolf, "timebin > 0") # alternatively
 
 # From: https://www.r-bloggers.com/diffusion-wiener-model-analysis-with-brms-part-iii-hypothesis-tests-of-parameter-estimates/
 # Thus, the resulting value is a probability (i.e., ranging from 0 to 1), 
@@ -61,31 +60,52 @@ hypothesis(brm_wolf, "timebin = 0") # alternatively
 # accuracy conditions in drift rate across word and non-word stimuli 
 # is .13, indicating that the evidence for a difference is at best weak. 
 
-plot(marginal_effects(brm_wolf, "timebin"))
+plot(marginal_effects(brm_wolf, "timebin"))[[1]] +
+    scale_x_discrete(
+        name ="Time period", 
+        limits = c("1800-1850","1850-1900","1900-1950","1950-2000")) +
+    scale_y_continuous(
+        name = "Probability of definite reference",
+        labels = percent,
+        limits = c(0, 1)) +
+    theme_bw() + 
+    theme(axis.text.x = element_text(angle = 25, vjust=0.6))
+
+
+## Red Riding Hood
 
 brm_rrh <- brm(rrh ~ timebin, data = df,
-               prior = c(set_prior("student_t(10, 0, 1)", class = "b")),
+               prior = c(set_prior("student_t(7, 0, 1)", class = "b")),
                family = "bernoulli", iter = 10000)
 summary(brm_rrh)
 plot(brm_rrh)
 
 # Next, what is the probability that timebin has no positive effect?
 mean(posterior_samples(brm_rrh, "timebin") < 0)
-hypothesis(brm_rrh, "timebin < 0") # alternatively
+hypothesis(brm_rrh, "timebin = 0") # alternatively
 # And finally, what is the probability that the definiteness of RRH has no effect?
 
-plot(marginal_effects(brm_rrh, "timebin"))
+plot(marginal_effects(brm_rrh, "timebin"))[[1]] +
+    scale_x_discrete(
+        name ="Time period", 
+        limits = c("1800-1850","1850-1900","1900-1950","1950-2000")) +
+    scale_y_continuous(
+        name = "Probability of definite reference",
+        labels = percent,
+        limits = c(0, 1)) + 
+    theme_bw() + 
+    theme(axis.text.x = element_text(angle = 25, vjust=0.6))
 
 ## Analysis of the effect of time on the presence of formulaic openings
 
 brm_opening <- brm(opening ~ timebin, data = df,
-                   prior = c(set_prior("student_t(10, 0, 1)", class = "b")),
+                   prior = c(set_prior("student_t(7, 0, 1)", class = "b")),
                    family = "bernoulli", iter = 10000)
 summary(brm_opening)
 plot(brm_opening)
 
-mean(posterior_samples(brm_opening, "timebin") < 0)
-hypothesis(brm_opening, "timebin < 0")
+mean(posterior_samples(brm_opening, "timebin") > 0)
+hypothesis(brm_opening, "timebin > 0")
 
 plot(marginal_effects(brm_opening, "timebin"))[[1]] +
     scale_x_discrete(
@@ -95,17 +115,18 @@ plot(marginal_effects(brm_opening, "timebin"))[[1]] +
         name = "Probability of formulaic opening",
         labels = percent,
         limits = c(0, 1)) + 
-    theme_bw()
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 25, vjust=0.6))
 
 exp(quantile(as.matrix(brm_opening)[,2], probs=c(.5, .025, .975)))
 
 
-## interaction
+## RRH without opening
 
-brm_rrh_opening <- brm(rrh ~ timebin * opening, data = df,
-               prior = c(set_prior("student_t(10, 0, 1)", class = "b")),
-               family = "bernoulli", iter = 10000)
-summary(brm_rrh_opening)
-plot(brm_rrh_opening)
-
-## Bayes model comparison rrh|tijd - rrh|tijd*opening 
+df_rrh <- df[df$opening == 0,]
+brm_rrh_no_opening <- brm(rrh ~ timebin, data = df_rrh,
+                          prior = c(
+                              set_prior("student_t(7, 0, 1)", class = "b")),
+                          family = "bernoulli", iter = 10000)
+summary(brm_rrh_no_opening)
+plot(brm_rrh_no_opening)
