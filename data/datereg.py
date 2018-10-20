ef0090189e22258782fdddf92a5d86fcd3d0eef7
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-10-20 13:05:49 +0200

    remove tpot functionalities

--- data/datereg.py
+++ data/datereg.py
@@ -5,11 +5,14 @@ import lxml.etree
 import numpy as np
 import pandas as pd
 
-import sklearn.linear_model as lm
+from sklearn.compose import TransformedTargetRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split, KFold
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import sklearn.metrics
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, FeatureUnion
+from sklearn.svm import LinearSVR
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, QuantileTransformer, RobustScaler
 
 
 parser = argparse.ArgumentParser()
@@ -28,21 +31,48 @@ for entry in os.scandir(args.corpus):
         stories[entry.name[:-4]] = ' '.join(text.itertext())
 
 knowns, dates = zip(*df[df['exact_date']][['id', 'year_corrected']].values)
-unknowns = df[~df['exact_date']]['id'].values
+unknowns, unkdates = zip(*df[~df['exact_date']][['id', 'year_corrected']].values)
 
-vectorizer = TfidfVectorizer(analyzer='char', norm='l2', ngram_range=(2, 5), min_df=2)
+vectorizer = FeatureUnion([
+    ('raw_chars', TfidfVectorizer(analyzer='char', ngram_range=(1, 4),
+                                  lowercase=False, use_idf=True, min_df=2)),
+    ('clean_chars', TfidfVectorizer(analyzer='char', ngram_range=(1, 4), min_df=2)),
+    ('puctuation', TfidfVectorizer(analyzer='word',  use_idf=False,
+                                   token_pattern=r'[^\w\s]+')),
+])
 
 X = [stories[id] for id in knowns]
-X = vectorizer.fit_transform(X)
-scaler = MinMaxScaler()
-y = scaler.fit_transform(np.array(dates).reshape(-1, 1))[:,0]
+vectorizer.fit(stories.values())
+X = vectorizer.transform(X)
+y = np.array(dates)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, shuffle=True, random_state=1983)
+X_train, X_test, y_train, y_test, id_train, id_test = train_test_split(
+    X, y, knowns, test_size=0.1, shuffle=True, random_state=1983)
 
-tpot = TPOTRegressor(generations=100, population_size=100, verbosity=2,
-                     config_dict='TPOT sparse', njobs=40, scoring='r2',
-                     periodic_checkpoint_folder='tpot')
-tpot.fit(X_train, y_train)
-print(tpot.score(X_test, y_test))
-tpot.export('tpot_datereg.py')
+# Average CV score on the training set was:0.8450677049610829
+regressor_pipeline = make_pipeline(
+    # SelectPercentile(score_func=f_regression, percentile=82),
+    # SelectPercentile(score_func=f_regression, percentile=16),
+    LinearSVR(C=25.0, dual=True, epsilon=0.0001, loss="epsilon_insensitive", tol=0.1)
+)
+
+y_scaler = RobustScaler()
+regressor = TransformedTargetRegressor(regressor=regressor_pipeline,
+                                       transformer=y_scaler)
+
+regressor.fit(X_train, y_train)
+test_pred = regressor.predict(X_test)
+
+abs_error = np.abs(y_test - test_pred)
+print("Mean Absolute Error", mean_absolute_error(y_test, test_pred))
+print("R2", r2_score(y_test, test_pred))
+
+regressor.fit(X, y)
+
+X_target = [stories[id] for id in unknowns]
+X_target = vectorizer.transform(X_target)
+
+preds = regressor.predict(X_target)
+print("Mean Absolute Error", mean_absolute_error(unkdates, preds))
+print("R2", r2_score(unkdates, preds))
+
