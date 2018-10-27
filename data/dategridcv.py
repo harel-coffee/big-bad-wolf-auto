e82c5f93cf6487ff9f777cd7aa33bd75859fe079
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-10-27 16:56:50 +0200

    stratified splits

--- data/dategridcv.py
+++ data/dategridcv.py
@@ -9,12 +9,12 @@ import pandas as pd
 
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_absolute_error
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.pipeline import FeatureUnion, Pipeline
-from sklearn.preprocessing import RobustScaler
-
+from sklearn.preprocessing import MinMaxScaler
+import unidecode
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--corpus', type=str)
@@ -35,38 +35,32 @@ for entry in os.scandir(args.corpus):
 knowns, dates = zip(*df[df['exact_date']][['id', 'year_corrected']].values)
 unknowns, unkdates = zip(*df[~df['exact_date']][['id', 'year_corrected']].values)
 
-X = [stories[id] for id in knowns]
+X = [unidecode.unidecode(' '.join(stories[id].split())) for id in knowns]
 y = np.array(dates)
-
-X_train, X_test, y_train, y_test, id_train, id_test = train_test_split(
-    X, y, knowns, test_size=0.1, shuffle=True, random_state=1983)
 
 pipeline = Pipeline([
     ('vec', TfidfVectorizer()),
     ('tt_svr', TransformedTargetRegressor(
-        regressor=Ridge(), transformer=RobustScaler()))
+        regressor=LinearRegression(), transformer=MinMaxScaler()))
 ])
 
 grid = {
-    'vec__analyzer': ['char'],
-    'vec__ngram_range': [(1, 4), (2, 4), (2, 5)],
-    'vec__min_df': [5, 10, 20],
+    'vec__analyzer': ['char', 'char_wb'],
+    'vec__ngram_range': [(2, 4), (2, 5)],
+    'vec__min_df': [1, 5, 10, 20, 50],
+    'vec__sublinear_tf': [True, False],
     'vec__max_df': [1.0, 0.9, 0.8, 0.7],
-    'vec__use_idf': [True, False],
-    'tt_svr__regressor__alpha': np.linspace(0.0001, 0.1, 20)
+    'vec__use_idf': [True, False]
 }
+
+bins = np.linspace(1840, 2020, 15)
+y_binned = np.digitize(y, bins)
+skf = StratifiedKFold(n_splits=5).split(np.zeros(y.shape[0]), y_binned)
 
 grid_cv = GridSearchCV(
     pipeline, grid, scoring=['r2', 'neg_mean_absolute_error'],
-    refit='r2', cv=10, n_jobs=args.n_jobs, verbose=1, error_score=np.nan)
+    refit='r2', cv=skf, n_jobs=args.n_jobs, verbose=1, error_score=np.nan)
 
-grid_cv.fit(X_train, y_train)
+grid_cv.fit(X, y)
 results = pd.DataFrame(grid_cv.cv_results_)
-results.to_csv("results-ridge.csv")
-
-test_pred = grid_cv.predict(X_test)
-
-abs_error = np.abs(y_test - test_pred)
-print("Mean Absolute Error", mean_absolute_error(y_test, test_pred))
-print("R2", r2_score(y_test, test_pred))
-
+results.to_csv("results-lr.csv")
