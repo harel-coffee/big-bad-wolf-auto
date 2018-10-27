3bb61a4aeca7aadad020dc21ede1257364a2fcef
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-10-27 17:53:58 +0200

    try again

--- data/tpot_config.py
+++ data/tpot_config.py
@@ -6,6 +6,14 @@ regressor_config_sparse = {
         'minimum_fraction': [0.05, 0.1, 0.15, 0.2, 0.25]
     },
 
+    'sklearn.ensemble.RandomForestRegressor': {
+        'n_estimators': [100],
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'bootstrap': [True, False]
+    },    
+
     'sklearn.feature_selection.SelectFwe': {
         'alpha': np.arange(0, 0.05, 0.001),
         'score_func': {
@@ -39,23 +47,14 @@ regressor_config_sparse = {
         'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
     },
 
-    'sklearn.linear_model.RidgeCV': {
+    'sklearn.linear_model.Ridge': {
+        'alpha': np.linspace(0.0001, 0.1, 20)
     },
 
     'sklearn.svm.LinearSVR': {
         'loss': ["epsilon_insensitive", "squared_epsilon_insensitive"],
-        'dual': [True, False],
         'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
         'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5., 10., 15., 20., 25.],
         'epsilon': [1e-4, 1e-3, 1e-2, 1e-1, 1.]
-    },
-
-    'xgboost.XGBRegressor': {
-        'n_estimators': [100],
-        'max_depth': range(1, 11),
-        'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
-        'subsample': np.arange(0.05, 1.01, 0.05),
-        'min_child_weight': range(1, 21),
-        'nthread': [1]
-    }
+    },    
 }
--- data/tpot_datereg.py
+++ data/tpot_datereg.py
@@ -1,21 +1,26 @@
 import argparse
 import os
+import warnings
+warnings.filterwarnings("ignore")
 
 import lxml.etree
 import numpy as np
 import pandas as pd
 
-from tpot import TPOTRegressor
+from sklearn.compose import TransformedTargetRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import FeatureUnion
-from sklearn.preprocessing import RobustScaler
-
-from tpot_config import regressor_config_sparse
-
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from tpot import TPOTRegressor
+import unidecode
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--corpus', type=str)
 parser.add_argument('--dataset', type=str)
+parser.add_argument('--n_jobs', type=int, default=1)
 args = parser.parse_args()
 
 df = pd.read_csv(args.dataset)
@@ -31,22 +36,25 @@ for entry in os.scandir(args.corpus):
 knowns, dates = zip(*df[df['exact_date']][['id', 'year_corrected']].values)
 unknowns, unkdates = zip(*df[~df['exact_date']][['id', 'year_corrected']].values)
 
-vectorizer = FeatureUnion([
-    ('raw_chars', TfidfVectorizer(analyzer='char', ngram_range=(1, 4),
-                                  lowercase=False, min_df=2)),
-    ('clean_chars', TfidfVectorizer(analyzer='char', ngram_range=(1, 4), min_df=2)),
-    ('puctuation', TfidfVectorizer(analyzer='word',  use_idf=False,
-                                   token_pattern=r'[^\w\s]+')),
-])
-
-vectorizer.fit(stories.values())
-X = [stories[id] for id in knowns]
-X = vectorizer.transform(X)
+X = [unidecode.unidecode(' '.join(stories[id].split())) for id in knowns]
 y = np.array(dates)
-scaler = RobustScaler()
+
+vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(2, 4), max_df=0.8,
+                             lowercase=False, sublinear_tf=True, min_df=20,
+                             use_idf=False)
+
+X = [stories[id] for id in knowns]
+X = vectorizer.fit_transform(X)
+y = np.array(dates)
+
+bins = np.linspace(1840, 2020, 15)
+y_binned = np.digitize(y, bins)
+skf = StratifiedKFold(n_splits=5).split(np.zeros(y.shape[0]), y_binned)
+
+scaler = MinMaxScaler()
 y = scaler.fit_transform(y.reshape(-1, 1)).squeeze()
 
-tpot = TPOTRegressor(generations=100, population_size=100, verbosity=3, cv=10,
+tpot = TPOTRegressor(generations=100, population_size=100, verbosity=3, cv=skf,
                      config_dict=regressor_config_sparse, n_jobs=30,
                      scoring='r2',
                      periodic_checkpoint_folder='tpot')
